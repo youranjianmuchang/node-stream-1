7f4f6bb83c155071eea3d423ad53589bbe85ca4b
JieZhan <178889339@qq.com>
2022-04-30 21:08:20 +0800

    first commit

---
+++ 1.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const stream = fs.createWriteStream('./big_file.txt')
+
+for(let i=1;i<=1000000;i++){
+    stream.write(`第${i}行内容，内容是=========================================\n`)
+}
+stream.end()
+console.log('done')
